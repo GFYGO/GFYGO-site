04b000028467440db864815e4e838cb752473110
GFYGO <gfygo_@outlook.com>
2024-02-29 17:17:21 +0800

    添加 从后端获取通知功能（1）

--- main.js
+++ main.js
@@ -1,67 +1,56 @@
 //变量
-var index_url = document.getElementById("https://rapid-cardinal-seriously.ngrok-free.app/"); // 为何要把URL作为元素的id？
+console.log("start")
+var notice = document.querySelector('.notice');
 var screenHeight = window.innerHeight;
 /////////////////////////////网络交互函数///////////////////////////////////////
 
-function get(type) {
-    fetch('http://localhost:81', {
-        headers: {
-            'information': type // 将信息作为头部传递给服务器
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // 在控制台中打印响应数据
-        document.getElementById('get_return').innerText = data.get_return;
-        // 可以在这里添加其他页面更新逻辑
-    });
+function get(information) {
+    fetch('http://localhost:81',{
+    headers:information => information.json()})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message); // 在控制台中打印
+        return data
+        });
 }
 
 
-// function main() {
-//     fetch('http://localhost:81/', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(),
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data.message); // 在控制台中打印添加成功的消息
-//         // 可以在这里添加其他页面更新逻辑
-//     });
-// }
-function post() {
-    fetch('http://localhost:81/postData', {
+function post(information) {
+    fetch('http://localhost:81/', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}) // 在JSON.stringify中提供需要发送的数据
+        headers: information=>information.json(),
+        body: JSON.stringify(),
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data.message); // 在控制台中打印成功的消息
-        // 可以在这里添加其他页面更新逻辑
+        console.log(data.message); // 在控制台中打印添加成功的消息
+        return data
     });
 }
+
+
 ///////////////////////////////页面函数（下面）/////////////////////////////////////
 
 //notice关闭按钮
 function closeNotice() {
-    var notice = document.querySelector('.notice');
-    notice.style.display = 'none'; // 隐藏通知
+    notice.style.display = 'none'; // 隐藏
 }
 
 //////////////////////////////////////////////////页面代码//////////////////////////////////////////////////
-get(notice)
-notice = data.get_return;
+function handleNotice() {
+    data = get({'type':'notice'})
+    var noticeElement = document.getElementById('notice');
+
+    if (data.notice === "none") {
+        closeNotice(); // 如果通知信息为 "none"，则调用关闭通知的函数
+    } else {
+        noticeElement.getElementsByTagName('h2')[0].getElementsByTagName('span')[0].innerText = data.h; // 更新标题内容
+        noticeElement.getElementsByTagName('a')[0].getElementsByTagName('span')[0].innerText = data.txt; // 更新链接文本
+    }
+}
 ///////////////////main/////////////////////////////////////////////////////////
 function mian(){
-    if (notice === 'none') {
-    closeNotice(); // 如果通知信息为 "none"，则调用关闭通知的函数
-    } 
+    handleNotice()
 }
 mian()
 
